ff5c2800b7860e8b01d6f45d96bf6d2c04323638
Jason Laning <jason.laning@gmail.com>
2017-04-25 23:32:10 -0400

    jl initial commit

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-// your code here!
+// need to fix makeWords to remove punctuation
+
+function makeWords (str) {
+	return str.split(" ");
+}
+
+function totalWords(words) {
+	$('.js-word-count').text(words.length);
+}
+
+function uniqueWords(words) {
+	var uniqueList = [];
+	for (var i = 0; i<words.length; i++) {
+		console.log($.inArray(words[i], uniqueList))
+		if (($.inArray(words[i], uniqueList)) === -1) {
+			uniqueList.push(words[i]);
+		}
+	}
+	$('.js-unique-words').text(uniqueList.length);
+}
+
+function averageWordLength(words) {
+	var total = 0;
+	for (var i = 0; i<words.length; i++) {
+		total += words[i].length;
+	}
+	$('.js-average-length').text(total / words.length + ' characters');
+}
+
+$(function() {
+	$('.js-text').submit(function(event) {
+		event.preventDefault();
+		$('.text-report').removeClass('hidden');
+		var words = makeWords($(this).find('textarea').val());
+		totalWords(words);
+		uniqueWords(words);
+		averageWordLength(words);
+	});
+});
